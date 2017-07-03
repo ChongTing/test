8781ffcc53fbdd3ec200c5b63998e24f13b0be7c
Zou Jian <chsword@126.com>
2017-07-03 18:36:42 +0800

    1

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public enum FirstControlType
+    {
+        Number,
+        DropDownList
+    }
+    public class FirstModel
+    {
+        public FirstControlType Type { get; set; }
+        public string Id { get; set; }
+
+    }
+    public class FirstController : Controller
+    {
+
+        // GET: First
+        public ActionResult Index()
+        {
+            var list = new List<FirstModel>
+            {
+                new FirstModel
+                {
+                    Id = "control1",
+                    Type = FirstControlType.Number
+                },
+                new FirstModel
+                {
+                    Id = "control2",
+                    Type = FirstControlType.DropDownList
+                }
+            };
+            return View(list);
+        }
+
+        public ActionResult GetData()
+        {
+            return Json(new[]
+            {
+                new {id = 1, name = "x1"},
+                new {id = 2, name = "x2"}
+            });
+        }
+    }
+}
---
+++ Controllers/SecondController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calc(CalcModel model)
+        {
+            return Json(model.Calc());
+        }
+    }
+
+    public class CalcModel
+    {
+        private Dictionary<string, Func<Expression, Expression, BinaryExpression>> _operatorDict
+            = new Dictionary<string, Func<Expression, Expression, BinaryExpression>>
+            {
+                {"+", Expression.Add},
+                {"-", Expression.Subtract},
+                {"*", Expression.Multiply},
+                {"/", Expression.Divide},
+            };
+        public double Calc()
+        {
+            var operExp = _operatorDict[Operator];
+            var const1 = Expression.Constant(Num1, Num1.GetType());
+            var const2 = Expression.Constant(Num2, Num2.GetType());
+
+            var method = Expression.Lambda<Func<double>>(operExp.Invoke(const1, const2));
+            return (double) method.Compile()();
+        }
+
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public string Operator { get; set; }
+    }
+}
